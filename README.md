26c21dda2855a7c2d4b9c0f727547447a3ec0164
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-09-26 16:17:14 -0500

    init commit - for deploy

---
+++ README.md
@@ -0,0 +1,19 @@
+# backend_7weeks_plusCapstone
+
+## Week 7 - Day 6 - Deployment
+
+_Prerequisite Knowledge: JavaScript Fundamentals, Node.js, Express, MongoDB, Mongoose, EJS, Bootstrap, REPL, BCrypt, JSONWebToken, VS Code_
+
+_Exercise Goals:_
+
+### MVP
+
+Title
+
+Description
+
+### _Optional_ Stretch Goal
+
+Title
+
+Description
---
+++ app.js
@@ -0,0 +1,64 @@
+//import dependencies
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+
+//initialize app and port
+const app = express()
+const port = 8080
+
+//import middleware
+const { requireAuth, checkUser } = require('./middlewares/authMiddleware')
+
+//connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+  .finally(() => {
+    console.log('API up and connected to the cloud. Happy Hacking! =)')
+  })
+
+//set EJS as view engine
+app.set('view engine', 'ejs')
+
+//serve static files
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.get('*', checkUser)
+app.get('/', (req, res) => {
+  res.redirect('/blog')
+})
+
+//import routes
+const adminRoutes = require('./routes/adminRoutes')
+const blogRoutes = require('./routes/blogRoutes')
+const authRoutes = require('./routes/authRoutes')
+
+//initialize routes
+app.use('/', authRoutes)
+app.use('/blog', blogRoutes)
+app.use('/admin', requireAuth, adminRoutes)
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' })
+})
+
+//port running
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
+
+//error handling
+app.use((req, res) => res.status(404).render('404', { title: 'Error Page' }))
